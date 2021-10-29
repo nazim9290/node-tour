041b443735efa06f405ce92e8e879275ccec5223
Md Nazim uddin <69738654+nazim9290@users.noreply.github.com>
2021-10-29 10:26:26 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const app = express();
+var cors = require("cors");
+const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+//midleware
+app.use(cors());
+app.use(express.json());
+
+const uri =
+  "mongodb+srv://ogro9290:3zpc5KYpujeWtMkr@cluster0.eanfw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("travel_bd");
+    const tourservice = database.collection("tour");
+    //custom api
+    app.get("/tour", async (req, res) => {
+      const cursor = tourservice.find({});
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    app.get("/home", (req, res) => {
+      res.send("mongo connect");
+    });
+  } finally {
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+
+//custom api
+app.get("/", (req, res) => {
+  res.send("Hell!");
+});
+
+//applesten
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
